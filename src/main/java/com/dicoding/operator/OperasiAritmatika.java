8f43d0145d1845363df31b646c64d0428f5d539b
brcadal <brucadal@gmail.com>
2025-07-19 10:55:55 +0700

    Aritmatika Hasil Sisa menggunakn anotasi %

--- src/main/java/com/dicoding/operator/OperasiAritmatika.java
+++ src/main/java/com/dicoding/operator/OperasiAritmatika.java
@@ -10,16 +10,23 @@ public class OperasiAritmatika {
         System.out.println("Operasi Pengurangan");
         int hasilPengurangan = 10 - 8;
         System.out.println("Hasil 10 - 8 = " + hasilPengurangan);
-
         System.out.println();
 
         System.out.println("Operasi Pengalian");
         int hasilPengalian = 10 * 20;
-        System.out.println("Hasil 10 * 10 = " + hasilPengalian);
+        System.out.println("Hasil 10 * 20 = " + hasilPengalian);
         System.out.println();
 
         System.out.println("Operasi Pembagian");
         int hasilPembagian = 20 / 10;
         System.out.println("Hasil 20 / 10 = " + hasilPembagian);
+        System.out.println();
+
+        System.out.println("Operasi Habis Bagi");
+        int habisBagi = 8 % 2;
+        System.out.println("Hasil 8 % 2 = " + habisBagi);
+        int hasilSisaLain = 9 % 2;
+        System.out.println("Hasil 9 % 2 = " + hasilSisaLain);
+        System.out.println();
     }
 }
