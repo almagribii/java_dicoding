c7936a13eb8e0ea73d2682cfef1554d68efc98d4
brcadal <brucadal@gmail.com>
2025-07-19 10:50:17 +0700

    Operasi Assignment dan Aritmatika

---
+++ src/main/java/com/dicoding/operator/OperasiAritmatika.java
@@ -0,0 +1,25 @@
+package com.dicoding.operator;
+
+public class OperasiAritmatika {
+    public static void main(String[] args) {
+        System.out.println("Operas Penjumalahan");
+        int hasilPenjumlahan = 5 + 1;
+        System.out.println("Hasil 5 + 1 = " + hasilPenjumlahan);
+        System.out.println();
+
+        System.out.println("Operasi Pengurangan");
+        int hasilPengurangan = 10 - 8;
+        System.out.println("Hasil 10 - 8 = " + hasilPengurangan);
+
+        System.out.println();
+
+        System.out.println("Operasi Pengalian");
+        int hasilPengalian = 10 * 20;
+        System.out.println("Hasil 10 * 10 = " + hasilPengalian);
+        System.out.println();
+
+        System.out.println("Operasi Pembagian");
+        int hasilPembagian = 20 / 10;
+        System.out.println("Hasil 20 / 10 = " + hasilPembagian);
+    }
+}
